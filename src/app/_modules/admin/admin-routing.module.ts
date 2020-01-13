2a88f8cad2ff252a40d381a40867c55830dec5fa
Farhad <jawad_aziz_farhad@yahoo.com>
2020-01-13 16:55:23 -0500

    Admin Panel added.

--- src/app/_modules/admin/admin-routing.module.ts
+++ src/app/_modules/admin/admin-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
-const adminRoutes : Routes = [
-  { path : 'home' , component : HomeComponent } ,
-  { path : '' , redirectTo: 'home' , pathMatch: 'full' }
+// const adminRoutes : Routes = [
+//   { path : 'home' , component : HomeComponent } ,
+//   { path : '' , redirectTo: 'home' , pathMatch: 'full' }
+// ]
+// @NgModule({
+//   imports: [RouterModule.forChild(adminRoutes)],
+//   exports : [RouterModule]
+// })
+
+// export class AdminRoutingModule {}
+
+export const adminRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  }, {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+        {
+      path: '',
+      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+  }]},
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
 ]
 @NgModule({
   imports: [RouterModule.forChild(adminRoutes)],
--- src/app/_modules/admin/admin.module.ts
+++ src/app/_modules/admin/admin.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { ComponentsModule } from '../shared/shared.module';
+import { SidebarModule } from './sidebar/sidebar.module';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { ToastrModule } from 'ngx-toastr';
+import { FooterModule } from './shared/footer/footer.module';
+import { FixedPluginModule } from './shared/fixedplugin/fixedplugin.module';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [AdminLayoutComponent ,HomeComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+    //NEW ONES
+    SidebarModule,
+    NavbarModule,
+    ToastrModule,
+    FooterModule,
+    FixedPluginModule
+
   ]
 })
 export class AdminModule { }
