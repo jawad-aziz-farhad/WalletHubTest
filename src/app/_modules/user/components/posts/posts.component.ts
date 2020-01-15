6ee9c6852b25b72c5238361490eb7e7182143845
Farhad <jawad_aziz_farhad@yahoo.com>
2020-01-15 17:17:31 -0500

    working on data parsing.

--- src/app/_modules/user/components/posts/posts.component.ts
+++ src/app/_modules/user/components/posts/posts.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
 import { IPost } from '../../_interfaces/post.interface';
-
+import { DataParserService } from 'src/app/_services/data-parser/data-parser.service';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -10,12 +10,17 @@ import { IPost } from '../../_interfaces/post.interface';
 export class PostsComponent implements OnChanges {
 
   @Input('posts') posts : Array<IPost>;
+  @Output() onPostClick$: any = new EventEmitter<IPost>();
   
-  constructor() { }
+  constructor(private dataParser: DataParserService) { }
 
   /* On Creating New Post, posts array will be modified and This Hook get Called*/
   ngOnChanges(changes : SimpleChanges){
     console.log('New Changes' , changes )
+  }
+
+  onPostClick(post){
+    this.dataParser.storage = post;
   }
 
 }
---
+++ src/app/_services/data-parser/data-parser.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataParserService {
+
+  public storage : any = {};
+
+  constructor() { }
+}
